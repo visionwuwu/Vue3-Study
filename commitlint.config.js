e820b5d58cc62fdd9c95dc32735d1e9faf596708
包武 <baow1541@fingard.com>
2023-04-05 23:31:22 +0800

    feat: 集成husky、lint-staged、commitlint保证代码规范

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+      'type-enum': [
+          2,
+          'always',
+          ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+      ],
+      'subject-full-stop': [0, 'never'],
+      'subject-case': [0, 'never'],
+  },
+};
